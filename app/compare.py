cd2e984041dfc56570688789a4b7520d28ec7371
Sebastien Thebert <sebthebert@gmail.com>
2023-10-21 00:52:19 +0200

    First minimal version

---
+++ app/compare.py
@@ -0,0 +1,22 @@
+from pkg_resources import parse_version
+
+def compare_equal(value, desired_value):
+    if value == desired_value:
+        return True
+    return False
+
+def compare_in(value, desired_value):
+    if value in desired_value:
+        return True
+    return False
+
+def compare_version_gte(value, desired_value):
+    if parse_version(str(value)) >= parse_version(str(desired_value)):
+        return True
+    return False
+
+compare_function = {
+    'equal': compare_equal,
+    'in': compare_in,
+    'version_greater_equal_than': compare_version_gte
+}
---
+++ app/compliance.py
@@ -0,0 +1,41 @@
+import mitogen
+import yaml
+from yaml.loader import SafeLoader
+
+from compare import compare_function
+from plugins.os import distribution_name, distribution_version, distribution_codename
+
+key_function = {
+    'distribution_name':            distribution_name,
+    'distribution_version':         distribution_version,
+    'distribution_codename':        distribution_codename,
+}
+
+with open('../rules/ubuntu.yml') as f:
+    rules = yaml.load(f, Loader=SafeLoader)
+
+@mitogen.main()
+def main(router):
+
+    containers = (
+        'ubuntu1804',
+        'ubuntu2004',
+        )
+
+    for host in containers:
+        z = router.docker(container=host, python_path='/usr/bin/python3')
+        print(f"{host}: {z.call(distribution_name)} {z.call(distribution_version)} ({z.call(distribution_codename)})")
+
+        for rule in rules['rules']:
+            id = rule['id']
+            key = rule['key']
+            if 'key_args' in rule:
+                key_args = rule['key_args']
+                value = z.call(key_function[key](key_args))
+            else:
+                value = z.call(key_function[key])
+            # print(value)
+            operator = rule['comparison']['operator']
+            desired = rule['comparison']['value']
+            status = compare_function[operator](value, desired)
+            print(f"[{id}] {key}: {value} {operator} {desired} ? => {('OK' if status else 'KO')}")
