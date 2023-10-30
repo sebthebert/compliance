fc5f2c853c4216e240f0288f264c6c3ff4b8133f
Sebastien Thebert <sebthebert@gmail.com>
2023-10-31 00:42:12 +0100

    Add 'os.linux_kernel_version' check

--- app/plugins/os.py
+++ app/plugins/os.py
@@ -6,14 +6,14 @@ import re
 
 PARSERS = {
     '/etc/lsb-release': {
-        'distribution': re.compile('^DISTRIB_ID=(.+)'),
-        'version':      re.compile('^DISTRIB_RELEASE=(.+)'),
-        'codename':     re.compile('^DISTRIB_CODENAME=(.+)') 
+        'distribution': re.compile(r'^DISTRIB_ID=(.+)'),
+        'version':      re.compile(r'^DISTRIB_RELEASE=(.+)'),
+        'codename':     re.compile(r'^DISTRIB_CODENAME=(.+)') 
     },
     '/etc/os-release': {
-        'distribution': re.compile('^ID=(.+)'),
-        'version':      re.compile('^VERSION_ID="?(.+)"?'),
-        'codename':     re.compile('^VERSION_CODENAME=(.+)') 
+        'distribution': re.compile(r'^ID="?(.*?)"?$'),
+        'version':      re.compile(r'^VERSION_ID="?([^"]+?)"?$'),
+        'codename':     re.compile(r'^VERSION_CODENAME="?([^"]+?)"?$')
     }
 }
 
@@ -27,8 +27,8 @@ def distribution_name() -> str:
                 for line in f.readlines():
                     match = re.match(regex['distribution'], line)
                     if match:
-                        return(match.group(1).lower())
-    return 'unknown'                  
+                        return match.group(1).lower()
+    return 'n/a'
 
 def distribution_version() -> str:
     """
@@ -40,8 +40,8 @@ def distribution_version() -> str:
                 for line in f.readlines():
                     match = re.match(regex['version'], line)
                     if match:
-                        return(match.group(1).lower())
-    return 'unknown'
+                        return match.group(1).lower()
+    return 'n/a'
 
 def distribution_codename() -> str:
     """
@@ -53,5 +53,16 @@ def distribution_codename() -> str:
                 for line in f.readlines():
                     match = re.match(regex['codename'], line)
                     if match:
-                        return(match.group(1).lower())
-    return 'unknown'
+                        return match.group(1).lower()
+    return 'n/a'
+
+def linux_kernel_version() -> str:
+    """
+    Function returning OS Linux Kernel version (ex.: '5.19.0-46-generic')
+    """
+    with open('/proc/version', encoding='utf-8') as f:
+        for line in f.readlines():
+            match = re.match(r'^Linux version (\S+)', line)
+            if match:
+                return match.group(1)
+    return 'n/a'
